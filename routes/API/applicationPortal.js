703b131ab0f5742e35aac6a66028fd3f894d5a5d
Nick Ramsay <NickRamsay1989@gmail.com>
2020-07-19 19:24:10 +1000

    Application now running on server with starter routes

---
+++ routes/API/applicationPortal.js
@@ -0,0 +1,6 @@
+//const router = require("express").Router();
+//const atlTrackController = require("../../controllers/atlTrackController");
+
+//router
+  //.route("/scrape-atl")
+  //.post(atlTrackController.scrapeATL);
---
+++ routes/API/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const atlTrackRoutes = require("./applicationPortal");
+
+// Book routes
+//router.use("/atl-track", atlTrackRoutes);
+
+module.exports = router;
